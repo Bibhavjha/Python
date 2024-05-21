4a9e78c466c04e9e5975547c41ab3224faef9aab
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-05-21 08:14:45 +0545

    Add files via upload

---
+++ nestedFor.py
@@ -0,0 +1,53 @@
+'''
+1
+1 2 
+1 2 3
+1 2 3 4
+1 2 3 4 5
+'''
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     print()
+'''1 2 3 4 5
+   1 2 3 4 
+   1 2 3
+   1 2
+   1
+    '''
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
+
+'''*
+**
+***
+****
+*****'''
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print('*',end='')
+#     print()
+
+'''
+5
+45
+345
+2345
+12345'''
+# for i in range(5,0,-1):
+#     for j in range(i,6):
+#         print(j,end='')
+#     print()
+
+for i in range(1,6):
+    for j in range(i,6):
+        print(j,end='')
+    print()
+
+
+
+
+
+     
