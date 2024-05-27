0ddad26e364b735952ed96eacbdb7ca4875f2dfc
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-05-27 07:49:32 +0545

    Add files via upload

---
+++ fString.py
@@ -0,0 +1,18 @@
+# f-string with a vriable
+age=20
+txt=f"ram is {age} years old"
+print(txt)
+# f-string with function
+x=50.89
+text=f"rounded values is {round(x)}"
+print(text)
+# f-string with operator
+a=5
+b=7
+output=f" multiplication of number = {a*b}"
+print(output)
+# f-string with modifier
+price=85
+text=f"price={price:.2f} rupess"
+print(text)
+
