97a2df7a1d88d0286a30e4f42c8542d96d2374ad
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-05-29 07:55:06 +0545

    Add files via upload

---
+++ List.py
@@ -0,0 +1,19 @@
+fruits=["apple","orange","grapes"]
+print(fruits)
+# no of items in list0
+print(len(fruits))
+# list constructor 
+mylist=list(("apple","orange","grapes"))
+print(mylist)
+
+# list indexing
+fruits=["apple","orange","grapes"]
+print(fruits[1])
+
+# negative indexing
+print(fruits[-1])
+
+# looping through a list 
+cities=["ktm","bkt","pkr"]
+for city in cities:
+    print(city)
---
+++ listSlicing.py
@@ -0,0 +1,10 @@
+mylist=['a','b','c','d','e']
+# start index
+mylist1=mylist[2:]
+print(mylist1)
+# end index
+mylist2=mylist[:3]
+print(mylist2)
+# only step
+mylist3=mylist[::2]
+print(mylist3)
