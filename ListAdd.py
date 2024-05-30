903d99557507d27927d07e0ee3a0aed487fa998f
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-05-30 08:11:15 +0545

    Add files via upload

---
+++ ListAdd.py
@@ -0,0 +1,11 @@
+# append
+fruits=["mango","litchi"]
+fruits.append("cherry")
+print(fruits)
+# insert
+fruits.insert(1,"orange") #if index is not specified then stores in last 
+print(fruits)
+# extend (can be acchived as well with concatenation)
+veg=["cabbage","onion"]
+fruits.extend(veg)
+print(fruits)
---
+++ ListRemove.py
@@ -0,0 +1,17 @@
+# using remove method
+nums=[1,2,3,4,4,2]
+nums.remove(2)
+print(nums)
+# using pop() method
+alpha=['a','b','c','d']
+alpha.pop(1)
+print(alpha)
+# using del keyword
+alpha=['a','b','c','d']
+del alpha[1]
+print(alpha)
+# del alpha removes whole list 
+# clear
+nums=[1,2,3]
+nums.clear()
+print(nums)
