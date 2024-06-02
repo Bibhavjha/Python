9bfac4a5eba279485aeb5f749819a3be49216ffa
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-06-02 19:10:17 +0545

    Add files via upload

---
+++ PackingUnpacking.py
@@ -0,0 +1,16 @@
+fruits=("apple","mango","orange") #packing
+a,b,c=fruits
+print(a)
+print(b)
+print(c)
+# 
+fruits=('apple','orange','mango')
+a,*b=fruits
+print(a)
+print(b)
+fruits=('apple','orange','mango','grapes','cherrey')
+a,*b,c=fruits
+print(a)
+print(b)
+print(c)
+
---
+++ TupleSlicing.py
@@ -0,0 +1,5 @@
+a=('a','b','c','d','e')
+b=a[:]
+print(b)
+c=a[::2]
+print(c)
