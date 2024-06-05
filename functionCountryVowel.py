2689df374a9930631b2ecea846f9db090f5ebe34
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-06-05 07:31:09 +0545

    Add files via upload

--- functionCountryVowel.py
+++ functionCountryVowel.py
@@ -1,9 +1,11 @@
-def vowel(country):
+# create a function that takes a list of country names and returns the
+# countries that end with vowel
+def vowel(countries):
     vowel=('a','e','i','o','u')
-    countries=[]
-    for i in country:
-        if country.endswith(vowel):
-            countries.append(country)
-    return country
-country=['usa','india','nepal']
-print(vowel(country))
+    a=[]
+    for country in countries:
+        if country.lower().endswith(vowel):
+         a.append(country)
+    return a    
+countries=['usa','germany','india','nepal']
+print(vowel(countries))
