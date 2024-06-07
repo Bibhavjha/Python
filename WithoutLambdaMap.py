7cbdf2ad86042f6e489381827e7c1c77c4146ba5
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-06-07 08:39:09 +0545

    Add files via upload

---
+++ WithoutLambdaMap.py
@@ -0,0 +1,5 @@
+def funct(n):
+    return n**2
+numbers=[1,2,3,4,5,6]
+result =map(funct,numbers)
+print(list(result))
---
+++ positonalArgument.py
@@ -0,0 +1,5 @@
+
+def myfunction(a,b,/):
+    print(a,b)
+myfunction(2,3)
+# myfunction(a=5,b=10) Genreates error 
