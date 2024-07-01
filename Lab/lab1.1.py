e235c8456d8aba0f4932a0ad004e8f8f83e8c1c5
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-07-01 19:41:08 +0545

    Create lab1.1.py

---
+++ Lab/lab1.1.py
@@ -0,0 +1,8 @@
+#WAP to input 2 numbers and perform addition /subtraction/multiplication and division
+a=int(input("enter 1st number "))
+b=int(input("enter 2nd number "))
+print("The sum of two numbers is ",a+b)
+print("The difference of two numbers is ",a-b)
+print("The product of two numbers is ",a*b)
+print("The division of two numbers is ",a/b)
+
