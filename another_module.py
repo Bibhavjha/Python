f564998d63933117cb036e1408362f920376cea0
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-07-08 18:43:45 +0545

    Add files via upload

---
+++ another_module.py
@@ -0,0 +1,9 @@
+# using import keyword 
+# import sample
+# print(sample.sample_varibale)
+# print(sample.say_hello("jhon"))
+# print(sample.add(2,3))
+
+#using form keyword
+from sample import add
+print(add(10,4))
---
+++ sample.py
@@ -0,0 +1,7 @@
+# create a sample module
+sample_varibale="this is a string varibale in sample.py"
+def say_hello(name):
+   return f"hello,{name} welcome to this simple module"
+def add(a,b):
+   return f"the sum of{a}+{b} is={a+b}"
+  
