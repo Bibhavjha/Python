50d765c7d947b9e93d00633bf55408760e307174
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-07-19 21:34:55 +0545

    Add files via upload

---
+++ FileHandling/matchCase.py
@@ -0,0 +1,28 @@
+# Create the follwoing program
+# 1.Write
+# 2. read
+# 3. append
+# Enter your choice
+while True:
+ print("1.Write\n2.read\n3.Append")
+ num=int(input("Enter your choice:"))
+
+ match num:
+    case 1:
+        f=open("m.txt",'w')
+        f.write(input("Enter you write:"))
+        f.close()
+    case 2:
+        f=open("m.txt",'r')
+        print(f.read())
+        f.close()
+    case 3:
+        f=open("m.txt",'a')
+        f.write(input("Enter what you want to write"))
+        f.close()
+    case other:
+        print("invalid number")
+        break
+        
+
+
---
+++ FileHandling/pathSepcify.py
@@ -0,0 +1,1 @@
+# f=open("d:\\bim5\\abc.txt",'w')
