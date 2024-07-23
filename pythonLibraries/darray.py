7ba38cc71c47c8a67352718b191bd553556b9891
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-07-23 22:20:47 +0545

    Add files via upload

---
+++ pythonLibraries/darray.py
@@ -0,0 +1,18 @@
+# 0-Darray
+import numpy as np
+arr=np.array(42)
+print(arr)
+# 1-Darray
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+# n-Darray
+import numpy as np
+a=np.array(42)
+b=np.array([1,2,3,4,5])
+c=np.array([[1,2,3],[4,5,6]])
+d=np.array([[[1,2,3],[4,5,6]],[[1,2,4],[5,6,7]]])
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
---
+++ pythonLibraries/datatypearray.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr=np.array([1,2,4,5],dtype='f')
+print(arr)
+print(arr.dtype)
