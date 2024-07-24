58e7989583dc259d10b5ecfb1da9030b8f6122be
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-07-24 20:20:51 +0545

    Add files via upload

---
+++ pythonLibraries/2-Darray.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr =np.array([[1,2,3],[4,5,6]])
+print(arr[0,2])
+print(arr[1,1])
+
+# negative indexing is also allowed 
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr[-2])
+arr1=np.array([[1,2,3],[4,5,6]])
+print(arr1[1,-2])
+print(arr1[-2,-1])
