271b81a3acf8c68be424d6b59481487c7c52d204
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-07-31 22:22:19 +0545

    Add files via upload

---
+++ pythonLibraries/MergeOuter.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data1={'StudentID':[101,102,103],'Name':['Ram','Hari','Sita']}
+data2={'StudentID':[101,102,104],'City':['Ktm','bkt','Pkr']}
+
+df1=pd.DataFrame(data1)
+df2=pd.DataFrame(data2)
+
+# merge using inner-Inner join
+df3=df1.merge(df2,on='StudentID',how="outer")
+print(df3)
