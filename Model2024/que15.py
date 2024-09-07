621494046fa46da4e00d264b6a9f9d0cfec1a6b1
Bibhavjha <118330372+Bibhavjha@users.noreply.github.com>
2024-09-07 20:51:06 +0545

    Create que15.py

---
+++ Model2024/que15.py
@@ -0,0 +1,7 @@
+# write a program using function to find the sum of any number passed to the function
+def myfunction(a,b):
+    print("the sum is", a+b)
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+myfunction(a,b)
+# try using another function where parameters are no defined
